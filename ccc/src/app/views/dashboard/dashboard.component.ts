fb1dd82278178bb54ff82793c83a03609454ed01
Freedom Khanyile <Freedom.Khanyile@betterlife.co.za>
2020-04-07 20:22:03 +0200

    Dashboard nav responsive.

--- ccc/src/app/views/dashboard/dashboard.component.ts
+++ ccc/src/app/views/dashboard/dashboard.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, OnDestroy } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { AccountService } from 'src/app/_services';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  mobileQuery: MediaQueryList;
+  fillerNav = Array.from({ length: 50 }, (_, i) => `Nav Item ${i + 1}`);
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher,
+    private accountService: AccountService) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
 
   ngOnInit() {
   }
-
+  signOut() {
+    this.accountService.signOut();
+  }
 }
